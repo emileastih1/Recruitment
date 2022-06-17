31b4732e5c6d5f44f8e4fd629c9bee7333069f96
Mariam <eng.nobia.86@gmail.com>
2022-06-17 21:25:28 +0200

    updated data.sql - Security InMemoryUser

--- Mariam_Ibrahim/CourseManagment/src/test/java/com/tasks/coursemanagment/endpoint/CourseControllerTest.java
+++ Mariam_Ibrahim/CourseManagment/src/test/java/com/tasks/coursemanagment/endpoint/CourseControllerTest.java
@@ -1,11 +1,18 @@
 package com.tasks.coursemanagment.endpoint;
 
+import com.tasks.coursemanagment.model.entity.Course;
+import com.tasks.coursemanagment.model.mapper.CourseMapper;
+import com.tasks.coursemanagment.repository.CourseRepository;
 import com.tasks.coursemanagment.service.CourseService;
 import javafx.application.Application;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mapstruct.factory.Mappers;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,11 +23,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource(
         locations = "classpath:application-integrationtest.yml")//in case test properties - recently not needed
 @AutoConfigureMockMvc
-class CourseControllerTest {
+public class CourseControllerTest {
     @MockBean
     CourseService courseService;
 
+    @Test
+    void when_send_id_update_and_return_course(){
 
+    }
 
 
 }
